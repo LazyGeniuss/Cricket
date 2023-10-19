9acbe356b61fb79bcc74b7a3e0123c14d983fdaf
Shreyash Silversky <shreyash.silversky@gmail.com>
2023-10-19 15:24:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <iframe
+        src="https://stream.crichd.vip/update/star1hi.php"
+        width="100%"
+        height="800px"
+        marginheight="0"
+        marginwidth="0"
+        scrolling="no"
+        frameborder="0"
+        allowfullscreen
+        allow="encrypted-media"
+      ></iframe>
     </div>
   );
 }
